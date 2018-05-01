de39ae84ae37b4d985fa94a8e7a3aba954cab5be
Oksana Tkachenko <oksana@crystax.net>
2018-05-02 01:13:06 +0300

    was changed all of thumbnails(now as buttons)

--- front_end_task_v1/index.js
+++ front_end_task_v1/index.js
@@ -1,6 +1,9 @@
+/*
 toggleOverlay = () => {
   document.getElementById('overlay').classList.toggle('overlay--open');
 }
+*/
+
 
 function on() {
   document.getElementById('overlay').style.display = 'flex';
