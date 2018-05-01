c9d34de91803a5e0c56ab41bf7d756dee653cc55
Oksana Tkachenko <oksana@crystax.net>
2018-05-01 23:10:27 +0300

    was changed button for opening and closing overlay

--- front_end_task_v1/index.js
+++ front_end_task_v1/index.js
@@ -2,3 +2,11 @@ toggleOverlay = () => {
   document.getElementById('overlay').classList.toggle('overlay--open');
 }
 
+function on() {
+  document.getElementById('overlay').style.display = 'flex';
+}
+
+function off() {
+  document.getElementById('overlay').style.display = 'none';
+}
+
